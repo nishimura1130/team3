496f7330a7ab8ed882e645a15c2237e61e184fda
hogehoge <hogehoge@example.com>
2021-08-02 16:49:17 +0900

    firstcommit

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -15,9 +15,20 @@ class AgendasController < ApplicationController
     @agenda.team = Team.friendly.find(params[:team_id])
     current_user.keep_team_id = @agenda.team.id
     if current_user.save && @agenda.save
-      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda') 
+      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda')
     else
       render :new
+    end
+  end
+
+  def destroy
+    @agenda = Agenda.find(params[:id])
+    if @agenda.team.owner_id == current_user.id || @agenda.user_id == current_user.id
+      @agenda.destroy
+      AgendaDestroy.agenda_destroy(@agenda).deliver
+      redirect_to dashboard_url,notice:"タスクを削除しました"
+    else
+      redirect_to dashboard_url,notice:"そのAgendaの作者もしくはそのAgendaに紐づいているTeamの作者（オーナー）のみ"
     end
   end
 
